79426ac37a54956cd5e0ad9e0f8f395a28e679da
dedosmedia <gerencia@dedosmedia.com>
2021-09-05 19:46:03 -0500

    Estructrua inicial con Create React App

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <Aventura/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Aventura.jsx
@@ -0,0 +1,32 @@
+
+import React from "react";
+
+
+
+class Aventura extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+
+        return (
+            <div>
+                <h1>Elije tu propia aventura:</h1>
+                <HistoriaItem/>
+            </div>
+        )
+        
+
+    }
+
+
+
+
+}
+
+
+export default Aventura;
